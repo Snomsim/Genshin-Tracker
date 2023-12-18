969c58da4476ca4a716b378cdb42b553d5442731
Snomsim <gozum.jas@sdca.edu.ph>
2023-12-18 09:51:50 +0800

    Commited

--- public/index.js
+++ public/index.js
@@ -1,10 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const rows = document.querySelectorAll('.table tbody tr');
     rows.forEach(row => {
-        const columns = row.getElementsByTagName('td');
-        console.log(columns);
-        console.log(`Title: ${columns[0]}`);
-        console.log(`Department: ${columns[2]}`)
+       const columns = row.getElementsByTagName('td');
+       console.log(columns[5]);
+       if(columns[5].textContent == 'completed'){
+          let removeButtons = columns[6].querySelectorAll(".btn-warning,.btn-danger");
+          removeButtons[0].classList.add('d-none');
+          removeButtons[1].classList.add('d-none'); 
+       }
     });
-});
+ });
+ 
+ viewButton = document.querySelectorAll('.view-ticket');
+viewButton.forEach(function(button){
+   button.addEventListener('click', function(){
+    let row = this.parentElement.parentElement;
+    let columns = row.getElementsByTagName('td');
+    let hoyoNo = row.getElementsByTagName('th')
 
+    console.log(`HOYO no: ${hoyoNo[0].textContent}`);
+    console.log(`Quality: ${columns[0].textContent}`);
+
+   });
+});
